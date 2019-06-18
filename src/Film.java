a7edfa0d1f7ba7ffd9e04fc7aaaf4154bd75158f
phisinger <philipsinger@mail.de>
2019-06-18 20:59:38 +0200

    Update classes Film, Schauspieler, Regisseur

--- src/Film.java
+++ src/Film.java
@@ -2,7 +2,80 @@ import java.util.Date;
 
 public class Film {
 
-    String titel;
-    String beschreibeung;
-    Date erscheinungsdatum;
+    private int id;
+    private String titel;
+    private String beschreibeung;
+    private String genre;
+    private Date erscheinungsdatum;
+    private int stimmen;
+    private float bewertung;
+
+
+//    Konstruktor
+    public void Film(int id, String titel, String beschreibeung, String genre, Date erscheinungsdatum, int stimmen, float bewertung)
+    {
+        this.id = id;
+        this.titel = titel;
+        this.beschreibeung = beschreibeung;
+        this.genre = genre;
+        this.erscheinungsdatum = erscheinungsdatum;
+        this.bewertung = bewertung;
+        this.stimmen = stimmen;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getBeschreibeung() {
+        return beschreibeung;
+    }
+
+    public void setBeschreibeung(String beschreibeung) {
+        this.beschreibeung = beschreibeung;
+    }
+
+    public Date getErscheinungsdatum() {
+        return erscheinungsdatum;
+    }
+
+    public void setErscheinungsdatum(Date erscheinungsdatum) {
+        this.erscheinungsdatum = erscheinungsdatum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getBewertung() {
+        return bewertung;
+    }
+
+    public void setBewertung(float bewertung) {
+        this.bewertung = bewertung;
+    }
+
+    public int getStimmen() {
+        return stimmen;
+    }
+
+    public void setStimmen(int stimmen) {
+        this.stimmen = stimmen;
+    }
 }
